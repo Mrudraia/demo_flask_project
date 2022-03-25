2c0a50e55827f4d8c82da7ac103248fdfcb02d9a
mrudraia <mrudraia@redhat.com>
2022-03-25 16:13:46 +0530

    additional apis added

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -6,7 +8,20 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Welcome to redhat"
+    
+@app.route("/greet", methods=["GET"])
+def greet():
+    response = {"message":"welcome to redhat, enjoy the opensource culture"}
+    return jsonify(response)
 
+@app.route('/post_json', methods=['POST'])
+def process_json():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.json
+        return json
+    else:
+        return 'Content-Type not supported!'
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
